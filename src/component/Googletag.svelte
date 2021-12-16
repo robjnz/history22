fafdd06bc5d0b4b514a054fbe88dd6453dbe0d71
robin <robjnz@outlook.com>
2021-12-16 11:12:37 +0000

    new history22

---
+++ src/component/Googletag.svelte
@@ -0,0 +1,11 @@
+<!-- Google Tag Manager (noscript) -->
+<noscript
+	><iframe
+		src="https://www.googletagmanager.com/ns.html?id=GTM-PJS56TL"
+		height="0"
+		width="0"
+		style="display:none;visibility:hidden"
+	/></noscript
+>
+<!-- End Google Tag Manager (noscript) -->
+
